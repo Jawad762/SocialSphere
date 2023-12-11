b048c955ad43679e29eaee90f72363c4bf8f891f
Jawad762 <mjawadzaiter@gmail.com>
2023-12-11 21:51:11 +0200

    adding notifications

--- controllers/notif.js
+++ controllers/notif.js
@@ -1,13 +1,28 @@
+import Tweet from "../../client/src/components/Tweet.jsx";
 import Notif from "../models/Notif.js";
 
 export const getNotifications = async (req, res, next) => {
     try {
-        const { userId } = req.params
-        const notifications = await Notif.find({ userId }).sort({
-            createdAt: -1
-        })
-        res.status(200).json(notifications)
+      const { userId } = req.params;
+  
+      const allNotifications = await Notif.find({ userId }).sort({
+        createdAt: -1,
+      });
+  
+      const notificationPromises = allNotifications.map(async (notif) => {
+        if (notif.type === 'like') {
+          const tweet = await Tweet.findById(notif.tweetId);
+          if (!tweet) return null;
+          return notif;
+        }
+      });
+  
+      const notifications = await Promise.all(notificationPromises);
+  
+      const filteredNotifications = notifications.filter((notif) => notif !== null);
+  
+      res.status(200).json(filteredNotifications);
     } catch (error) {
-        res.status(404).json({ error: error.message })
+      res.status(404).json({ error: error.message });
     }
-}
+  };
