2ac8787e1655ec439b3cd8d17416ee163627f60c
Jawad762 <mjawadzaiter@gmail.com>
2023-12-11 19:30:28 +0200

    adding notifications

--- controllers/notif.js
+++ controllers/notif.js
@@ -3,7 +3,9 @@ import Notif from "../models/Notif.js";
 export const getNotifications = async (req, res, next) => {
     try {
         const { userId } = req.params
-        const notifications = await Notif.find({ userId })
+        const notifications = await Notif.find({ userId }).sort({
+            createdAt: -1
+        })
         res.status(200).json(notifications)
     } catch (error) {
         res.status(404).json({ error: error.message })
